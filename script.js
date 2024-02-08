80cd794d95ede982b7c72e1c41b016f212ee03f0
Dane knight <knight1seven@gmail.com>
2024-02-08 13:03:29 -0500

    added resultText variable and if conditions to checkButton.addEventListner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const checkButton = document.getElementById("check-btn");
 const inputText = document.getElementById("text-input");
-
+const resultText = document.getElementById("result");
 
 const textInput = () => {
 
@@ -9,4 +9,15 @@ const textInput = () => {
 const isTextPalindrome = () => {
 
 };
+
+
+
+checkButton.addEventListener( "click", (inputText) => {
+    if (inputText === null) {
+        window.alert("Please input a value");
+    } if (inputText === "A") {
+
+    }
+} )
+
 
