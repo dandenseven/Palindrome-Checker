70f01ebe5590418d2fc5418ae46eb3b541f0b5fa
Dane knight <knight1seven@gmail.com>
2024-02-07 20:17:10 -0500

    added variables for check button, input text and two functions

---
+++ script.js
@@ -0,0 +1,12 @@
+const checkButton = document.getElementById("check-btn");
+const inputText = document.getElementById("text-input");
+
+
+const textInput = () => {
+
+}
+
+const isTextPalindrome = () => {
+
+};
+
