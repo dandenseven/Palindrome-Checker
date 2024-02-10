1a6b934957e32e4c8afb66ef84e7c1e127f9d6a5
Dane knight <knight1seven@gmail.com>
2024-02-09 21:17:42 -0500

    modify the isTextPalindrome function

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const checkButton = document.getElementById("check-btn");
 const inputText = document.getElementById("text-input");
 const resultText = document.getElementById("result");
 
+
 const textInput = () => {
 
 }
 
 const isTextPalindrome = () => {
-
+    const isAPalindrome = string.lower(textInput).replace(/\W+/g, " ")
+    if (string === isAPalindrome)
+    return isAPalindrome
 };
 
 
@@ -16,8 +19,11 @@ checkButton.addEventListener( "click", (inputText) => {
     if (inputText === null) {
         window.alert("Please input a value");
     } if (inputText === "A") {
-
+        resultText.innerText = "A is a palindrome"
+    } if (inputText === "eye") {
+        resultText.innerText = "eye is a palindrome"
     }
 } )
+
 
 
